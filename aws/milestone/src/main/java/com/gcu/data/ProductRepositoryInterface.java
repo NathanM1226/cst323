bcf98459b7e5e698acba0353eda8b6e0dc1388cd
Nathan McMahon <nathanmc1226@gmail.com>
2024-03-28 10:15:18 -0700

    Updates

--- aws/milestone/src/main/java/com/gcu/data/ProductRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.gcu.data;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import com.gcu.models.ProductEntity;
-
-public interface ProductRepositoryInterface extends CrudRepository<ProductEntity, Long>{
-    List<ProductEntity> findByProductNameContainingIgnoreCase(String searchTerm);
-}
--- aws/milestone/src/main/java/com/gcu/data/RegistrationDataInterface.java
+++
@@ -1,9 +0,0 @@
-package com.gcu.data;
-
-import com.gcu.models.LoginModel;
-
-public interface RegistrationDataInterface {
-    public boolean isAuthenticated(LoginModel model);
-    public boolean isTaken(LoginModel model);
-    public int addOne(LoginModel newUser);
-}
